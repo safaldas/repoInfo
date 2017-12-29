12c33b29587ef094255e9a185e9ef8624d07ea5a
safal <safaldasg@gmail.com>
2017-12-29 12:27:22 +0530

    angular animation and material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 
-
+import { MaterialModules } from './material.modules';
 @NgModule({
-  declarations: [
-    AppComponent,
-    SearchComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, SearchComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MaterialModules],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
