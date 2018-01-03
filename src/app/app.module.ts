c0cd8cfe6c733fb21f4e57b3792480f342dcd152
safal <safaldasg@gmail.com>
2018-01-03 14:25:58 +0530

    fix merge conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,23 +8,26 @@ import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import { UserInfoComponent } from './user-info/user-info.component';
 import { FormsModule } from '@angular/forms';
+import { MaterialModules } from './material.modules';
 import { HttpModule } from '@angular/http';
-import {UserInfoService } from './user-info/user-info.service';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
+import { UserInfoService } from './user-info/user-info.service';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 @NgModule({
   declarations: [AppComponent, SearchComponent, UserInfoComponent, PageNotFoundComponent],
-  imports: [BrowserModule,
-            BrowserAnimationsModule,
-            FormsModule,
-            HttpModule,
-            RouterModule.forRoot([
-              { path: 'search', component: SearchComponent},
-              { path: 'userinfo/:full_name', component: UserInfoComponent},
-              { path: '' , redirectTo:'search', pathMatch: 'full' },
-              { path: '**' , component: PageNotFoundComponent }
-              ])],
-  providers: [SearchService,
-    UserInfoService],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MaterialModules,
+    HttpModule,
+    RouterModule.forRoot([
+      { path: 'search', component: SearchComponent },
+      { path: 'userinfo/:full_name', component: UserInfoComponent },
+      { path: '', redirectTo: 'search', pathMatch: 'full' },
+      { path: '**', component: PageNotFoundComponent }
+    ])
+  ],
+  providers: [SearchService, UserInfoService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
