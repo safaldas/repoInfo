79aac036370572b958490172ba977a3f64c29862
safal <safaldasg@gmail.com>
2018-01-04 16:58:19 +0530

    reducer defined

---
+++ src/app/auth/reducers/loginpage-reducer.ts
@@ -0,0 +1,56 @@
+import { AuthActions } from './../actions/action-creators';
+import { ActionTypes } from './../actions/action-types';
+import { fail } from 'assert';
+
+/*
+ * interface for the login page state
+ * the state has pending and error statuses
+ */
+
+export interface State {
+  error: string | null;
+  pending: boolean;
+}
+/*
+ * Setting the initial state
+ */
+export const initialState: State = {
+  error: null,
+  pending: false
+};
+
+/*
+ * defining the reducer
+ */
+
+export function reducer(
+  state = initialState,
+  action: AuthActions
+): State {
+  switch (action.type) {
+    case ActionTypes.LOGIN: {
+      return {
+        ...state,
+        error: null,
+        pending: true
+      };
+    }
+    case ActionTypes.LOGIN_SUCCESS: {
+      return {
+        ...state,
+        error: null,
+        pending: false
+      };
+    }
+    case ActionTypes.LOGIN_FAIL: {
+      return {
+        ...state,
+        error: action.payload,
+        pending: false
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+}
