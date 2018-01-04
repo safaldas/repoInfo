529d3dd6a83443bab33873ab8182213494bf3000
safal <safaldasg@gmail.com>
2018-01-04 16:58:01 +0530

    login action creators for success fail redirect defined

--- src/app/auth/actions/action-creators.ts
+++ src/app/auth/actions/action-creators.ts
@@ -1,7 +1,35 @@
 import { Action } from '@ngrx/store';
 import { ActionTypes } from './action-types';
-import { Authenticate } from '../../models/user';
+import { Authenticate, User } from '../../models/user';
+
 export class Login implements Action {
   readonly type = ActionTypes.LOGIN;
   constructor(public payload: Authenticate) {}
 }
+
+export class LoginSuccess implements Action {
+  readonly type = ActionTypes.LOGIN_SUCCESS;
+
+  constructor(public payload: { user: User }) {}
+}
+
+export class LoginFailure implements Action {
+  readonly type = ActionTypes.LOGIN_FAIL;
+
+  constructor(public payload: any) {}
+}
+
+export class LoginRedirect implements Action {
+  readonly type = ActionTypes.LOGIN_REDIRECT;
+}
+
+export class Logout implements Action {
+  readonly type = ActionTypes.LOGOUT;
+}
+
+export type AuthActions =
+  | Login
+  | LoginSuccess
+  | LoginFailure
+  | LoginRedirect
+  | Logout;
