6927fafd263cc4e1492a3469af43ca943d59f45f
safal <safaldasg@gmail.com>
2018-01-04 11:34:31 +0530

    for reacctive forms formGroup to work import forms module and reactive formms module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './components/login/login.component';
+import { LoginpageComponent } from './containers/loginpage/loginpage.component';
 
+const COMPONENTS = [LoginComponent, LoginpageComponent];
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [LoginComponent]
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  exports: COMPONENTS,
+  declarations: COMPONENTS
 })
-export class AuthModule { }
+export class AuthModule {}
