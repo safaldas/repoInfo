1571f4ba278dea5cbad4a911c04ca3606662e9a0
safal <safaldasg@gmail.com>
2018-01-05 09:48:03 +0530

    auth reducer created

---
+++ src/app/auth/reducers/auth-reducer.ts
@@ -0,0 +1,40 @@
+import { User } from '../../models/user';
+import { AuthActions } from '../actions/action-creators';
+import { ActionTypes } from '../actions/action-types';
+/*
+ * Auth reducer keeps the data of the user in the state
+ *  
+ */
+
+export interface State {
+  user: User | null;
+  loggedIn: boolean;
+}
+
+export const initialState: State = {
+  user: null,
+  loggedIn: false
+};
+/*
+ * This reducer sets Login Success and Login Failure
+ */
+
+export function reducer(
+  state = initialState,
+  action: AuthActions
+): State {
+  switch (action.type) {
+    case ActionTypes.LOGIN_SUCCESS: {
+      return {
+        ...state,
+        loggedIn: true,
+        user: action.payload.user
+      };
+    }
+    case ActionTypes.LOGOUT: {
+      return initialState;
+    }
+    default:
+      return state;
+  }
+}
